c0ac124f2cd65464a317803c0050e4d63ecfeb79
juhtolent <julia.tolentino@hotmail.com>
2024-04-05 16:08:03 -0300

    adding parameters and first descriptions

--- streamlit/homepage_dashboard.py
+++ streamlit/homepage_dashboard.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import plotly.express as px
+from parameters import *
 from st_pages import Page, Section, add_page_title, show_pages
 
 
@@ -10,10 +11,16 @@ st.set_page_config(page_title="Portfólio",
                    page_icon="📊",
                    initial_sidebar_state="expanded",
                    menu_items={
-                       'Get Help/Ajuda': 'https://www.extremelycoolapp.com/help',
-                       'About': ""
-                   }
-                   )
+                       'About': "I'm Julia, a data analyst passionate about extracting insights from data. This portfolio showcases my skills in data cleaning, analysis, and visualization, helping you make data-driven decisions."
+                   })
 
-st.title("Bem-vindo/a!")
+with st.container():
+    col1, col2 = st.columns([2, 8])
 
+with col1:
+    st.markdown(info['Photo'], unsafe_allow_html=True)
+
+with col2:
+    st.title("Hi! I'm Julia Tolentino!")
+    st.markdown("I'm Julia, a data analyst with a passion for exploring and extracting insights from data. I've been working in the field of data analysis for the past two years, where I've gained valuable experience in various aspects of data science, including data cleaning, data wrangling, data visualization, and data modeling.")
+    st.markdown("This portfolio showcases my skills in data cleaning, analysis, and visualization, helping you make data-driven decisions.")
---
+++ streamlit/parameters.py
@@ -0,0 +1,9 @@
+info = {
+    "Pronoun": "she/her",
+    "Name": "Julia",
+    "Full_Name": "Julia Tolentino",
+    "Intro": "A Tech Educator and AI Enthusiast at cognitiveclass.ai",
+    "About": "Hi there! I'm Julia, a data analyst with a passion for exploring and extracting insights from data. I've been working in the field of data analysis for the past two years, where I've gained valuable experience in various aspects of data science, <b> including data cleaning, data wrangling, data visualization, and data modeling. </b> <br>",
+    "Photo": """<a href=\"https://www.linkedin.com/in/julia-tolentino/\"><img src=\"https://media.licdn.com/dms/image/D4D03AQFYaKGDodA1mQ/profile-displayphoto-shrink_200_200/0/1712324948018?e=1717632000&v=beta&t=gRK6_tZybCQwuClLzQWEGJgbd24XJ9PPzjqTyVfdGYc" height=\"200\"   alt=\"Profile Photo of a woman with black and green hair in front of several buildings in Buenos Aires\" title=\"Profile\"></a>""",
+    "Email": "vicky.kuo.contact@gmail.com"
+}
