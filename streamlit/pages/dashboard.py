f819a2bf3169c3d0c61256f728271b59d11de02d
juhtolent <julia.tolentino@hotmail.com>
2024-04-22 10:24:44 -0300

    adding dataframes

--- streamlit/pages/dashboard.py
+++ streamlit/pages/dashboard.py
@@ -3,8 +3,132 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 
-st.set_page_config(page_title='Portfolio',
+st.set_page_config(page_title='Dashboard',
                    layout='wide',
                    page_icon='📊',
                    initial_sidebar_state='expanded'
                    )
+
+# ----- dataframes and cache
+
+
+@st.cache_data
+def import_category_2019():
+    df_2019 = pd.read_excel(
+        'data/2019_MTur_Categorization.xlsx', header=3)
+
+    # renaming to english and to standardize
+    df_2019.rename({
+        'UF': 'State',
+        'Município': 'City',
+        'Região Turística': 'Tourist Region',
+        'Domésticos': 'Domestic Tourists',
+        'Internacionais': 'International Tourists',
+        # Quantity of establishments related to tourism
+        'Estabelecimentos': 'Establishments',
+        'Empregos': 'Jobs',  # Quantity of jobs related to tourism
+        'Arrecadação de Impostos': 'Tax Revenue',
+        'Categoria': 'Category',
+        'Código Município': 'City Code'}, axis='columns', inplace=True)
+    return df_2019
+
+
+df_2019 = import_category_2019()
+
+
+@st.cache_data
+def import_category_2016():
+    df_2016 = pd.read_csv(
+        'data/2016_MTur_Categorization.csv', delimiter=';')
+
+    # renaming to english and to standardize
+    df_2016.rename({
+        # the five official regions of Brazil based on shared characteristics, like climate or vegetation.
+        'MACRO': 'Macro-Region',
+        'UF': 'State',
+        'MUNICIPIO': 'City',
+        'REGIAO_TURISTICA': 'Tourist Region',
+        'QUANTIDADE_VISITAS_ESTIMADA_NACIONAL': 'Domestic Tourists',
+        'QUANTIDADE_VISITAS_ESTIMADA_INTERNACIONAL': 'International Tourists',
+        'QUANTIDADE_ESTABELECIMENTOS': 'Establishments',
+        'QUANTIDADE_EMPREGOS': 'Jobs',
+        'CLUSTER': 'Category',
+        'CODIGO_MUNICIPIO': 'City Code'}, axis='columns', inplace=True)
+    return df_2016
+
+
+df_2016 = import_category_2016()
+
+
+@st.cache_data
+def import_category_2017():
+    df_2017 = pd.read_csv(
+        'data/2017_MTur_Categorization.csv', delimiter=';')
+
+    # renaming to english and to standardize
+    df_2017.rename({
+        'Macro Região': 'Macro-Region',
+        'UF': 'State',
+        'Município': 'City',
+        'Região': 'Tourist Region',
+        'Demanda Doméstica': 'Domestic Tourists',
+        'Demanda Internacional': 'International Tourists',
+        'Qtd. Estabelecimentos Hospedagem': 'Establishments',
+        'Qtd. Empregos Hospedagem': 'Jobs',
+        'Cluster 2017 (Categoria)': 'Category',
+        'Código Município': 'City Code'}, axis='columns', inplace=True)
+
+    df_2017['International Tourists'] = df_2017['International Tourists'].replace(
+        '.', '', regex=True)  # there are numbers with a '.'
+    df_2017['International Tourists'] = pd.to_numeric(
+        df_2017['International Tourists'])
+
+    df_2017['Domestic Tourists'] = df_2017['Domestic Tourists'].replace(
+        '.', '', regex=True)
+    df_2017['Domestic Tourists'] = pd.to_numeric(df_2017['Domestic Tourists'])
+
+    return df_2017
+
+
+df_2017 = import_category_2017()
+
+# adding a column to each df and concating the dataframes
+df_2019['Year'] = 2019
+df_2016['Year'] = 2016
+df_2017['Year'] = 2017
+
+df = pd.concat([df_2019, df_2016, df_2017])
+
+
+@st.cache_data
+def import_latlong():
+    # adding lat/long to the dataframe
+    df_latlong = pd.read_csv(
+        'data/BR_cities_latlong.csv', delimiter=',')
+
+    # renaming to english and to standardize
+    # renaming to english and to standardize
+    df_latlong.rename({
+        'codigo_ibge': 'City Code',
+        'nome': 'City',
+        'latitude': 'latitude',
+        'longitude': 'longitude',
+        'capital': 'Is Capital City',  # each state has a capital in Brazil,
+        'codigo_uf': 'State Code',
+        'siafi_id': 'Siafi ID',  # code for government administrative processes
+        'ddd': 'Area Code',
+        'fuso_horario': 'Time Zone'
+    }, axis='columns', inplace=True)
+    return df_latlong
+
+
+df_latlong = import_latlong()
+
+# merging both dfs
+df = pd.merge(df,
+              # selecting only lat/long information
+              df_latlong[['City Code', 'latitude', 'longitude']],
+              how='left',
+              on='City Code')
+
+st.dataframe(df)
