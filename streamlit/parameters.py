0adc9a14dd34a2e7a3ae34abd234acb2c0b1bcd6
Julia Tolentino <julia.tolentino@hotmail.com>
2024-07-06 23:26:30 -0300

    Update parameters.py

--- streamlit/parameters.py
+++ streamlit/parameters.py
@@ -3,7 +3,7 @@ info = {
     "Pronouns": "she/her/ela/dela",
     "Intro": "Data Analyst",
     "About": "Hi there! I'm Julia, a data analyst with a passion for exploring and extracting insights from data. I've been working in the field of data analysis for the past two years, where I've gained valuable experience in various aspects of data science, including data cleaning, data wrangling, data visualization, and data modeling.",
-    "Photo": """<a href=\"https://www.linkedin.com/in/julia-tolentino/\"><img src=\"https://media.licdn.com/dms/image/D4D03AQFYaKGDodA1mQ/profile-displayphoto-shrink_200_200/0/1712324948018?e=1717632000&v=beta&t=gRK6_tZybCQwuClLzQWEGJgbd24XJ9PPzjqTyVfdGYc" height=\"200\" alt=\"Profile Photo of a woman with black and green hair in front of several buildings in Buenos Aires\" title=\"Profile\" ></a>""",
+    "Photo": """<a href=\"https://www.linkedin.com/in/julia-tolentino/\"><img src=\"https://media.licdn.com/dms/image/D4D03AQFYaKGDodA1mQ/profile-displayphoto-shrink_200_200/0/1712324948018?e=1726099200&v=beta&t=7aCpKxEkj9SqnwJwSw3JgbdbIb46aHuYLIOor317qX8" height=\"200\" alt=\"Profile Photo of a woman with black and green hair in front of several buildings in Buenos Aires\" title=\"Profile\" ></a>""",
     "Email": "julia.mtolentino@hotmail.com",
     "Linkedin": "https://www.linkedin.com/in/julia-tolentino/",
     "Github": "https://github.com/juhtolent"
